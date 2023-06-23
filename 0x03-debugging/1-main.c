ddb360603ff4c11b8c20d0c87f27078b0c5ce52b
Amribrahimfathy <amr_core20duo2yahoo.com>
2023-06-22 19:46:06 -0700

    .

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,7 @@
 /**
  * main - causes an infinite loop
  * Return: 0
-*/
+ */
 
 int main(void)
 
@@ -13,6 +13,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
