47610f855017c6dece69b95326006cf8e70d70bc
Amribrahimfathy <amr_core20duo2yahoo.com>
2023-07-03 15:34:59 -0700

    .

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,18 +3,17 @@
 
 /**
  * print_diagsums - Entry point
- * @a: input
- * @size: input
+ * @a: 2d array of int types
+ * @size: size of array (square)
  * Return: Always 0 (success)
  */
 void print_diagsums(int *a, int size)
 {
 	int i, n, sum1 = 0, sum2 = 0;
+for (i = 0; i <= (size * size); i = i + size + 1)
+	sum1 = sum2 + a[i];
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		sum1 = sum2 + a[i];
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-		sum2 = sum2 + a[n];
+for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	sum2 = sum2 + a[n];
 	printf("%d, %d\n", sum1, sum2);
 }
